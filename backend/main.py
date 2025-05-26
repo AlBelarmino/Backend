1fd48b6a02b7b87f97af73668f996eec795c01ab
AlBelarmino <johnalbelarmino01@gmail.com>
2025-05-26 23:38:28 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://frontend-bjah-q625btsln-albelarminos-projects.vercel.app"],
+    allow_origins=["https://frontend-bjah-git-main-albelarminos-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
