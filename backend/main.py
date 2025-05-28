0ed8bb01cba34e4bf599c69dcdc12be648a769ca
AlBelarmino <johnalbelarmino01@gmail.com>
2025-05-28 20:18:00 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://frontend-nu-azure-26.vercel.app","frontend-git-main-albelarminos-projects.vercel.app"],  # Adjust as needed
+    allow_origins=["https://frontend-nu-azure-26.vercel.app","frontend-git-main-albelarminos-projects.vercel.app","https://autopayslip.netlify.app"],  # Adjust as needed
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
